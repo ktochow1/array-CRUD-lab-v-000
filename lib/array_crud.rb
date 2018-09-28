08082aaa5b2ae49da26af2c13a1eb84e51c87b15
ktochow1 <ktochow1@binghamton.edu>
2018-09-28 20:06:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["hello", "am", "are", "a"]
+  array = ["hello", "are", "am", "a"]
+  array[]
   
 end
 
