5278fc6cc38c18a8965e32365e634a2078e47203
ktochow1 <ktochow1@binghamton.edu>
2018-09-28 19:58:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   animals = ["dog"]
-  animals.unshift("cat")
+  animals.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
